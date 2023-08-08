653f228f50b01aa3206bdc51a825b4508c34bf36
sixmillions <liubw95@163.com>
2023-08-08 16:15:36 +0800

    fix: 去掉模板自带的定时器

--- main.ts
+++ main.ts
@@ -86,7 +86,7 @@ export default class MyPlugin extends Plugin {
     this.addSettingTab(new SampleSettingTab(this.app, this))
 
     // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000))
+    // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000))
   }
 
   onunload() {}
